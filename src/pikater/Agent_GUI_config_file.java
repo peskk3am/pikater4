d8eeb2b90be96439c083dd13cb9a2b86ff99e9bb
Klara Peskova <ustav@seznam.cz>
2013-11-26 13:29:57 +0100

    Agent_GUI_config_file - clean version, some changes in Agent_GUI

--- src/pikater/Agent_GUI_config_file.java
+++ src/pikater/Agent_GUI_config_file.java
@@ -3,14 +3,18 @@ package pikater;
 import jade.content.lang.Codec.CodecException;
 import jade.content.onto.OntologyException;
 import jade.core.AID;
+import jade.domain.FIPAAgentManagement.FailureException;
 import jade.domain.FIPAException;
 import jade.util.leap.Iterator;
 import jade.util.leap.List;
 
 import java.io.IOException;
 
+import org.jdom.Document;
+import org.jdom.Element;
 import org.jdom.JDOMException;
 
+import org.jdom.input.SAXBuilder;
 import pikater.ontology.messages.*;
 
 public class Agent_GUI_config_file extends Agent_GUI {
@@ -92,8 +96,6 @@ public class Agent_GUI_config_file extends Agent_GUI {
 	protected void mySetup() {
 		setDefault_error_rate(0.01);
 
-		doWait(10000);
-
 		configFileName = getArgumentValue(CONFIG);
 		try {
 			 log("xml file: " + System.getProperty("user.dir") + System.getProperty("file.separator") + configFileName);
@@ -108,92 +110,134 @@ public class Agent_GUI_config_file extends Agent_GUI {
 		
 	} // end mySetup
 
+    protected void getProblemsFromXMLFile(String fileName) throws JDOMException, IOException {
+        SAXBuilder builder = new SAXBuilder();
+        log("GetProblemsFromXMLFile: " + path + fileName);
+        Document doc = builder.build("file:" + path + fileName);
+        Element root_element = doc.getRootElement();
 
-    private Boolean _test(){
-        try {
-            log("J48 options: " + getOptions("J48"));
-
-            int newId = createNewProblem("1000", "after_each_computation", "yes", "test");
-            addAgentToProblem(newId, null, "MultilayerPerceptron", "-L 0.4 -D -M ? -H ?,?");
-            addAgentToProblem(newId, null, "RBFNetwork", "-B 4");
-            addAgentToProblem(newId, null, "?", null);
-
-            addDatasetToProblem(newId, "iris.arff", "iris.arff", null, "evaluation_only", "train_test");
-
-            getAgentOptions("mp1");
-
-            log("Agent types: " + offerAgentTypes());
-        } catch (CodecException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            return false;
-        } catch (OntologyException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            return false;
-        } catch (FIPAException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            return false;
+        java.util.List _end_pikater_when_finished = root_element.getChildren("hasta_la_vista_baby");
+        if (_end_pikater_when_finished.size() > 0){
+            end_pikater_when_finished = true;
+            // take into consideration only the first one
+            if ( ((Element)_end_pikater_when_finished.get(0)).getAttributeValue("shutdown_database").equals("true")){
+                shutdown_database = true;
+            }
         }
 
-        return true;
-    }
+        // return all children by name
+        java.util.List _problems = root_element.getChildren("experiment");
+        java.util.Iterator p_itr = _problems.iterator();
+        while (p_itr.hasNext()) {
+            Element next_problem = (Element) p_itr.next();
 
-    private Boolean _test_loading(){
+            int p_id = createNewProblem(next_problem.getAttributeValue("timeout"),
+                    next_problem.getAttributeValue("get_results"),
+                    next_problem.getAttributeValue("save_results"),
+                    next_problem.getAttributeValue("name"));
 
-        Agent a = new Agent();
-        a.setName("1_RBFNetwork0_2011-05-17_09-01-32.263");
-        a.setGui_id("pokusny oziveny agent");
+            java.util.List evaluation_method = next_problem.getChildren("evaluation");
+            if (evaluation_method.size() == 0){
+                throw new JDOMException("evaluation tag missing.");
+            }
+            if (evaluation_method.size() > 1) {
+                throw new JDOMException("more than one evaluation tags found.");
+            }
 
-        /*
-        try {
-            loadAgent("1_RBFNetwork0_2011-05-17_09-01-32.263", null);
-        } catch (FIPAException e2) {
-            // TODO Auto-generated catch block
-            e2.printStackTrace();
-            return false;
+            java.util.Iterator em_itr = evaluation_method.iterator();
+            Element next_evaluation_method = (Element) em_itr.next();
+            addEvaluationMethodToProblem(p_id, next_evaluation_method.getAttributeValue("name"));
+
+            java.util.List _evaluation_method_options = next_evaluation_method.getChildren("parameter");
+            java.util.Iterator emo_itr = _evaluation_method_options.iterator();
+            while (emo_itr.hasNext()) {
+                Element next_option = (Element) emo_itr.next();
+                addEvaluationMethodOption(p_id, next_option.getAttributeValue("name"),
+                        next_option.getAttributeValue("value"));
+            }
+
+            java.util.List method = next_problem.getChildren("method");
+            java.util.Iterator m_itr = method.iterator();
+            if (method.size() == 0) {
+                throw new JDOMException("method tag missing.");
+            }
+            if (method.size() > 1) {
+                throw new JDOMException("more than one method tags found.");
+            }
+            Element next_method = (Element) m_itr.next();
+            addMethodToProblem(p_id, next_method.getAttributeValue("name"));
+
+            java.util.List _search_options = next_method.getChildren("parameter");
+            java.util.Iterator so_itr = _search_options.iterator();
+            while (so_itr.hasNext()) {
+                Element next_option = (Element) so_itr.next();
+                addSearchOption(p_id, next_option.getAttributeValue("name"),
+                        next_option.getAttributeValue("value"));
+            }
+
+            java.util.List dataset = next_problem.getChildren("dataset");
+            java.util.Iterator ds_itr = dataset.iterator();
+            while (ds_itr.hasNext()) {
+                Element next_dataset = (Element) ds_itr.next();
+                int d_id = addDatasetToProblem(p_id,
+                        next_dataset.getAttributeValue("train"),
+                        next_dataset.getAttributeValue("test"),
+                        next_dataset.getAttributeValue("label"),
+                        next_dataset.getAttributeValue("output"),
+                        next_dataset.getAttributeValue("mode"));
+
+                java.util.List metadata = next_dataset.getChildren("metadata");
+                if (metadata.size() > 0) {
+                    java.util.Iterator md_itr = metadata.iterator();
+                    Element next_metadata = (Element) md_itr.next();
+
+                    addMetadataToDataset(d_id,
+                            next_dataset.getAttributeValue("train"),
+                            next_metadata.getAttributeValue("missing_values"),
+                            next_metadata.getAttributeValue("number_of_attributes"),
+                            next_metadata.getAttributeValue("number_of_instances"),
+                            next_metadata.getAttributeValue("attribute_type"),
+                            next_metadata.getAttributeValue("default_task"));
+                }
+            }
+
+            java.util.List recommender = next_problem.getChildren("recommender");
+            java.util.Iterator r_itr = recommender.iterator();
+
+            if (recommender.size() != 0) {
+                while (r_itr.hasNext()) {
+                    Element next_recommender = (Element) r_itr.next();
+                    addRecommenderToProblem(p_id, next_recommender.getAttributeValue("name"));
+                }
+            }
+
+            java.util.List _agents = next_problem.getChildren("agent");
+            java.util.Iterator a_itr = _agents.iterator();
+            while (a_itr.hasNext()) {
+                Element next_agent = (Element) a_itr.next();
+
+                String agent_name = next_agent.getAttributeValue("name");
+                String agent_type = next_agent.getAttributeValue("type");
+                int a_id = -1;
+                try {
+                    a_id = addAgentToProblem(p_id, agent_name, agent_type, null);
+                } catch (FailureException e) {
+                    logError(e.getLocalizedMessage());
+                }
+
+                java.util.List _options = next_agent.getChildren("parameter");
+                java.util.Iterator o_itr = _options.iterator();
+                while (o_itr.hasNext()) {
+                    Element next_option = (Element) o_itr.next();
+                    addOptionToAgent(p_id, a_id,
+                            next_option.getAttributeValue("name"),
+                            next_option.getAttributeValue("value"),
+                            next_option.getAttributeValue("lower"),
+                            next_option.getAttributeValue("upper"),
+                            next_option.getAttributeValue("number_of_values_to_try"),
+                            next_option.getAttributeValue("set"));
+                }
+            }
         }
-        */
-        return true;
-    }
-
-
-    private Boolean _test_loading2(){
-        // test of loading a saved agent
-
-        String agentName = "1_RBFNetwork1_2011-05-22_23-17-33.112";
-
-        Agent a = new Agent();
-        a.setName(agentName);
-        a.setGui_id("pokusny oziveny agent");
-
-        Data d = new Data();
-        d.setMode("test_only");
-        d.setTest_file_name("data/files/772c551b8486b932aed784a582b9c1b1");
-        d.setTrain_file_name("data/files/772c551b8486b932aed784a582b9c1b1"); // weather
-
-        // d.setTest_file_name("data/files/25d7d5d689042a3816aa1598d5fd56ef");
-        // d.setTrain_file_name("data/files/25d7d5d689042a3816aa1598d5fd56ef"); // iris
-        d.setExternal_test_file_name("weather.arff");
-        d.setExternal_train_file_name("weather.arff");
-        d.setOutput("predictions");
-
-        Task t = new Task();
-        t.setAgent(a);
-        t.setData(d);
-
-        // t.setId("pokusny task pro pokusneho oziveneho agenta");
-        // t.setProblem_id("neni soucasti zadneho problemu");
-
-        Execute ex = new Execute();
-        ex.setTask(t);
-
-        try {
-            loadAgent(agentName, ex, null);
-        } catch (FIPAException e2) {
-            // TODO Auto-generated catch block
-            e2.printStackTrace();
-            return false;
-        }
-        return true;
-    }
-
+    } // end getProblemsFromXMLFile
 }
